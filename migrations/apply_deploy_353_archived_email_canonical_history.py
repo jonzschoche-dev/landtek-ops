c4f16b4e8895d64b7ade6e67329e4c953daed858
Jonathan Zschoche <jonzschoche@gmail.com>
2026-06-06 21:40:47 +0800

    deploy_353b: fix deploy_353 syntax
    Auto-tagged by landtek_git_routine.sh on Mac.

--- migrations/apply_deploy_353_archived_email_canonical_history.py
+++ migrations/apply_deploy_353_archived_email_canonical_history.py
@@ -8,7 +8,7 @@ import os
 import subprocess
 import sys
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from landtek_core import db
 
 
